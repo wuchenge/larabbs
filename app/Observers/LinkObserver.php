f494cbfcadbf135cb61c8f24cfb164ba662b6440
无尘阁 <ligui0506@126.com>
2018-10-30 15:00:13 +0800

    Sidebar links

---
+++ app/Observers/LinkObserver.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @Author: wuchenge
+ * @Date: 2018-10-30 14:58:34
+ */
+
+namespace App\Observers;
+
+use App\Models\Link;
+use Cache;
+
+class LinkObserver
+{
+    // 在保存时清空 cache_key 对应的缓存
+    public function saved(Link $link)
+    {
+        Cache::forget($link->cache_key);
+    }
+}
