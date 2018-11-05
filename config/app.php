1b6901a7e9d66091740ccd38e4dfff989a33a3ad
无尘阁 <ligui0506@126.com>
2018-11-05 20:41:25 +0800

    verificationCode store

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // 第三方注册
+        App\Providers\EasySmsServiceProvider::class,
+
     ],
 
     /*
--- routes/api.php
+++ routes/api.php
@@ -2,27 +2,12 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1', function ($api) {
-    $api->get('version', function () {
-        return response('this is version v1');
-    });
-});
-
-$api->version('v2', function ($api) {
-    $api->get('version', function () {
-        return response('this is version v2');
-    });
+$api->version('v1', [
+    'namespace' => 'App\Http\Controllers\Api'
+], function ($api) {
+    // 短信验证码
+    $api->post('verificationCodes', 'VerificationCodesController@store')
+        ->name('api.verificationCodes.store');
 });
