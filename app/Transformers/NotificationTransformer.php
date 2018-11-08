6f67804883dfd6b5fd6fafc3174842e71ec6988f
无尘阁 <ligui0506@126.com>
2018-11-08 20:18:16 +0800

    notifications index

---
+++ app/Transformers/NotificationTransformer.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @Author: wuchenge
+ * @Date: 2018-11-08 20:16:03
+ */
+
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+use Illuminate\Notifications\DatabaseNotification;
+
+class NotificationTransformer extends TransformerAbstract
+{
+    public function transform(DatabaseNotification $notification)
+    {
+        return [
+            'id' => $notification->id,
+            'type' => $notification->type,
+            'data' => $notification->data,
+            'read_at' => $notification->read_at ? $notification->read_at->toDateTimeString() : null,
+            'created_at' => $notification->created_at->toDateTimeString(),
+            'updated_at' => $notification->updated_at->toDateTimeString(),
+        ];
+    }
+}
