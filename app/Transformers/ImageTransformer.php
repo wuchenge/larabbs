2709b06b2587d9c3c92f5eade3964f536ec46a67
无尘阁 <ligui0506@126.com>
2018-11-08 16:31:08 +0800

    user update

---
+++ app/Transformers/ImageTransformer.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @Author: wuchenge
+ * @Date: 2018-11-08 16:23:27
+ */
+
+namespace App\Transformers;
+
+use App\Models\Image;
+use League\Fractal\TransformerAbstract;
+
+class ImageTransformer extends TransformerAbstract
+{
+    public function transform(Image $image)
+    {
+        return [
+            'id' => $image->id,
+            'user_id' => $image->user_id,
+            'type' => $image->type,
+            'path' => $image->path,
+            'created_at' => $image->created_at->toDateTimeString(),
+            'updated_at' => $image->updated_at->toDateTimeString(),
+        ];
+    }
+}
