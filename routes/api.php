15ad977d7b3b498258f4e12cf0f90bdeedf788d2
无尘阁 <ligui0506@126.com>
2018-11-08 19:48:34 +0800

    topic delete api

--- routes/api.php
+++ routes/api.php
@@ -61,6 +61,9 @@ $api->version('v1', [
 
             $api->patch('topics/{topic}', 'TopicsController@update')
                 ->name('api.topics.update');
+
+            $api->delete('topics/{topic}', 'TopicsController@destroy')
+                ->name('api.topics.destroy');
         });
     });
 });
